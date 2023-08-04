c21982d3f911b36a536b20c920051dbb13359cb8
Pavel Shlapak <letohx@gmail.com>
2023-08-04 23:35:11 +0300

    fix: burger menu

--- library/index.js
+++ library/index.js
@@ -6,45 +6,27 @@ console.log('1. Вёрстка соответствует макету. Шири
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header-nav');
     const burgerBg = document.querySelector('.open-burger-bg');
-    const burgerBg2 = document.querySelector('.open-burger-bg2');
     const burgerIcon = document.querySelector('.header-burger');
     const burgerLine1 = document.querySelector('.burger-line-first');
     const burgerLine2 = document.querySelector('.burger-line-second');
     const burgerLine3 = document.querySelector('.burger-line-third');
     const burgerItems = document.querySelectorAll('.header-item');
+
+    const toggleBurger = () =>{
+        menu.classList.toggle('header-nav-active');
+        burgerBg.classList.toggle('open-burger-bg-active');
+        burgerIcon.classList.toggle('header-burger-active');
+        burgerLine1.classList.toggle('burger-line-first-active');
+        burgerLine2.classList.toggle('burger-line-second-active');
+        burgerLine3.classList.toggle('burger-line-third-active');
+    };
    
-    burgerItem.addEventListener('click', () => {
-        menu.classList.toggle('header-nav-active');
-        burgerBg.classList.toggle('open-burger-bg-active');
-        burgerBg2.classList.toggle('open-burger-bg2-active');
-        burgerIcon.classList.toggle('header-burger-active');
-        burgerLine1.classList.toggle('burger-line-first-active');
-        burgerLine2.classList.toggle('burger-line-second-active');
-        burgerLine3.classList.toggle('burger-line-third-active');
-    });
-    burgerBg.addEventListener('click', () => {
-        menu.classList.toggle('header-nav-active');
-        burgerBg.classList.toggle('open-burger-bg-active');
-        burgerBg2.classList.toggle('open-burger-bg2-active');
-        burgerIcon.classList.toggle('header-burger-active');
-        burgerLine1.classList.toggle('burger-line-first-active');
-        burgerLine2.classList.toggle('burger-line-second-active');
-        burgerLine3.classList.toggle('burger-line-third-active');
-    });
-    burgerBg2.addEventListener('click', () => {
-        menu.classList.toggle('header-nav-active');
-        burgerBg.classList.toggle('open-burger-bg-active');
-        burgerBg2.classList.toggle('open-burger-bg2-active');
-        burgerIcon.classList.toggle('header-burger-active');
-        burgerLine1.classList.toggle('burger-line-first-active');
-        burgerLine2.classList.toggle('burger-line-second-active');
-        burgerLine3.classList.toggle('burger-line-third-active');
-    });
+    burgerItem.addEventListener('click', () => toggleBurger());
+    burgerBg.addEventListener('click', () => toggleBurger());
     burgerItems.forEach((item) => {
         item.addEventListener('click', () => {
-            menu.classList.toggle('header-nav-active');
+            menu.classList.remove('header-nav-active');
             burgerBg.classList.remove('open-burger-bg-active');
-            burgerBg2.classList.remove('open-burger-bg2-active');
             burgerIcon.classList.remove('header-burger-active');
             burgerLine1.classList.remove('burger-line-first-active');
             burgerLine2.classList.remove('burger-line-second-active');
