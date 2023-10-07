a26b8ac6aac552880e798a6e8710391af7882105
Pavel Shlapak <letohx@gmail.com>
2023-10-07 14:08:50 +0300

    feat: add function merge left

--- random-game/index.js
+++ random-game/index.js
@@ -1,23 +1,63 @@
-const setGame = () => {
-    let area = [
-        [0, 0, 0, 0],
-        [0, 2, 0, 0],
-        [4, 0, 4, 0],
-        [0, 8, 0, 0]
-    ]
+let area= [
+    [2, 2, 2, 2],
+    [2, 2, 2, 2],
+    [8, 8, 8, 8],
+    [4, 4, 8, 8]
+];
+
+const updateBoard = () => {
+    document.querySelector(".area").innerText = "";
 
     area.forEach((row, rowIndex) => {
-        row.forEach((elem, elemIndex) => {
-            let tile = document.createElement("div");
-            let num = area[rowIndex][elemIndex];   
-            tile.innerText = "";    
-            tile.classList.value = "";
-            tile.classList.add("tile");
-            tile.classList.add(`_${num}`);
-            tile.innerText = (num > 0) ? num : '';
-            document.querySelector(".area").append(tile);
+        row.forEach((_, colIndex) => {
+            let cell = document.createElement("div"); 
+            updateCell(cell, rowIndex, colIndex);
+            document.querySelector(".area").append(cell);
         });        
     });
 }
+updateBoard();
 
-setGame();
+function updateCell(cell, rowIndex, colIndex) {
+    const num = area[rowIndex][colIndex];
+    cell.innerText = "";    
+    cell.classList.value = "";
+    cell.classList.add("cell");
+    cell.classList.add(`${rowIndex}-${colIndex}`);
+    cell.classList.add(`_${num}`);
+    cell.innerText = (num > 0) ? num : '';
+}
+
+const deleteZeros = (row) => row.filter(num => num != 0);
+
+const mergeCells = (row) => {
+    row = deleteZeros(row);
+
+    row.forEach((num, index) => {
+        if (num === row[index + 1]) {
+            row[index] *= 2;
+            row[index + 1] = 0;            
+        }
+    });
+
+    row = deleteZeros(row);
+
+    while (row.length < 4) {
+        row.push(0);
+    }
+
+    return row;
+}
+
+function moveLeft() {
+    area.forEach((row, rowIndex) => {
+        area[rowIndex] = mergeCells(row);
+    });
+    updateBoard();
+}
+
+document.addEventListener("keyup", (e) => {
+    if (e.code === "ArrowLeft") {
+        moveLeft();
+    }
+})
